1ec45e526409e36f84007692ac25f31467dc5ee9
Halouani <88207779+HalouaniMohamed@users.noreply.github.com>
2025-04-09 16:13:19 +0200

    Update ShopfrontApplication.java

--- shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplication.java
+++ shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplication.java
@@ -16,6 +16,6 @@ public class ShopfrontApplication {
 
     @Bean(name = "stdRestTemplate")
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(); 
     }
 }
