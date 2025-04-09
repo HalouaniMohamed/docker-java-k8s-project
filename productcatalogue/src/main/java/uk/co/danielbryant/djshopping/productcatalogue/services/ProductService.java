1376bc59547c6430f8c7691be84fbbb59ecd9c2f
Halouani <88207779+HalouaniMohamed@users.noreply.github.com>
2025-04-09 16:26:21 +0200

    Update ProductService.java

--- productcatalogue/src/main/java/uk/co/danielbryant/djshopping/productcatalogue/services/ProductService.java
+++ productcatalogue/src/main/java/uk/co/danielbryant/djshopping/productcatalogue/services/ProductService.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class ProductService {
 
     //{productId, Product}
-    private Map<String, Product> fakeProductDAO = new HashMap<>();
+    private Map<String, Product> fakeProductDAO = new HashMap<>(); 
 
     public ProductService() {
         fakeProductDAO.put("1", new Product("1", "Widget", "Premium ACME Widgets", new BigDecimal(1.20)));
