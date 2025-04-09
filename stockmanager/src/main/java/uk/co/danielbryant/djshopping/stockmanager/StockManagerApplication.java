f134470db497b56f3887b1d961448e0ae287c64d
Halouani <88207779+HalouaniMohamed@users.noreply.github.com>
2025-04-09 16:26:49 +0200

    Update StockManagerApplication.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockManagerApplication.class, args);
+        SpringApplication.run(StockManagerApplication.class, args); 
     }
 }
